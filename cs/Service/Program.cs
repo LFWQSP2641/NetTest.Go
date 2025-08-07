05c105679d869ac46510d1813f47508cb26e5d91
LFWQSP2641 <liuyunbo2641@gmail.com>
2025-08-07 23:01:01 +0800

    Add C# support

---
+++ cs/Service/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+using System;
+
+public class Program
+{
+    [DllImport("netcore.dll", EntryPoint = "DnsRequest", CharSet = CharSet.Ansi)]
+    public static extern IntPtr DnsRequest(string dnsServer, string domain, string recordType, string recordClass);
+
+    [DllImport("netcore.dll", EntryPoint = "FreeCString", CharSet = CharSet.Ansi)]
+    public static extern void FreeCString(IntPtr ptr);
+
+    public static void Main(string[] args)
+    {
+        IntPtr result = DnsRequest("223.5.5.5:53", "dns.alidns.com", "A", "IN");
+        if (result != IntPtr.Zero)
+        {
+            string? resultStr = Marshal.PtrToStringUTF8(result);
+            if (resultStr != null)
+            {
+                Console.WriteLine("Result length: " + resultStr.Length);
+                Console.WriteLine("Result: " + resultStr);
+            }
+            else
+            {
+                Console.WriteLine("Result string is null.");
+            }
+            FreeCString(result);
+        }
+        else
+        {
+            Console.WriteLine("No result returned.");
+        }
+    }
+}
